5a0150b59950298c875b691c43324eb41c03b19c
laura.jmr <laura.juergensmeier@gmail.com>
2019-01-26 11:06:03 +0100

    initialized Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,54 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+
 public class Server {
-	a
+
+	private ServerSocket serverSocket;
+	private Socket client;
+	private int port;
+	private static boolean stop;
+
+	public Server(int port1) {
+		port = port1;
+		stop = false;
+
+		try {
+			serverSocket = new ServerSocket(port);
+
+			serverSocket.setSoTimeout(1000000);
+		} catch (SocketException e) {} catch (IOException e) {}
+	}
+
+	public void activate() {
+
+		System.out.println("waitint for client at " + port);
+
+		try {
+			client = serverSocket.accept();
+			//DataInputStream input = new DataInputStream(client.getInputStream());
+			DataOutputStream output = new DataOutputStream(client.getOutputStream());
+
+			//System.out.println(client.getRemoteSocketAddress() + ": " + input.readUTF());
+			System.out.println(client.getRemoteSocketAddress());
+			output.writeUTF("ola");
+
+			System.out.println("handled");
+
+			client.close();
+		} catch (Exception e) {
+			System.out.println("stopped");
+			stop = true;
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Server server = new Server(8080);
+
+		while (stop == false) {
+			server.activate();
+		}
+	}
 }
