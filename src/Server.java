a0058c301a072292ec4c6dc1ff2f0c5e37c10234
laura.jmr <laura.juergensmeier@gmail.com>
2019-01-27 22:01:46 +0100

    save client inputs

--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,27 @@
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
 
 public class Server {
 
+	public static ArrayList<String> clientInputs;
+
 	public static void main (String[] args) throws IOException {
 		ServerSocket server = new ServerSocket(9090);
+		clientInputs = new ArrayList();
+
+		if (clientInputs.isEmpty() != true) {
+			for (int i = 0; i < clientInputs.toArray().length; i++) {
+				clientInputs.remove(clientInputs.get(i));
+			}
+		}
 
 		System.out.println("server started\nwaiting for connection on port: " + 9090 + "...");
 
 		while (true) {
 			sendMessageToClient("ola", server);
-			readMessageFromClient(server);
+			saveMessageFromClient(server);
+			readMessageFromClient(0);
 		}
 	}
 
@@ -21,16 +32,22 @@ public class Server {
 		}
 	}
 
-	private static void readMessageFromClient (ServerSocket server) throws IOException {
+	private static void saveMessageFromClient (ServerSocket server) throws IOException {
 		Socket client = server.accept();
 		InputStreamReader input = new InputStreamReader(client.getInputStream());
 		BufferedReader reader = new BufferedReader(input);
 		String line = reader.readLine();
 
 		while (!line.isEmpty()) {
-			System.out.println(line);
+			String in = line;
+			clientInputs.add(in);
 
 			line = reader.readLine();
 		}
+	}
+
+	private static void readMessageFromClient (int position) {
+		Object[] inputs = clientInputs.toArray();
+		System.out.println(inputs[position]);
 	}
 }
